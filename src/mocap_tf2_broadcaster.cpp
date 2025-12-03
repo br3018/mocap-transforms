4dd2db2029b9ae408e49e3ce44dc25a5246f8dad
Shockwave PC <iosm.robotics@sa.catapult.org.uk>
2025-12-03 14:10:52 +0000

    mocap fixes

--- src/mocap_tf2_broadcaster.cpp
+++ src/mocap_tf2_broadcaster.cpp
@@ -24,40 +24,45 @@ class MocapTF2Broadcaster : public rclcpp::Node
             // Initialize transform broadcaster.
             tf_broadcaster_ =
                 std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-            
-            // Define lambda function for handling incoming RigidBodies messages.
-            auto handle_rigid_bodies = [this](const std::shared_ptr<mocap4r2_msgs::msg::RigidBodies> msg){
-                geometry_msgs::msg::TransformStamped t;
 
-                // Process each rigid body in the message using for-each loop.
-                for (const auto& rigidbody : msg->rigidbodies) {
-                    // Populate the header of the TransformStamped message.
-                    t.header.stamp = this->get_clock()->now();
-                    t.header.frame_id = "world";
-                    t.child_frame_id = rigidbody.rigid_body_name;
-                    // Populate the transform data.
-                    t.transform.translation.x = rigidbody.pose.position.x;
-                    t.transform.translation.y = rigidbody.pose.position.y;
-                    t.transform.translation.z = rigidbody.pose.position.z;
-                    t.transform.rotation.x = rigidbody.pose.orientation.x;
-                    t.transform.rotation.y = rigidbody.pose.orientation.y;
-                    t.transform.rotation.z = rigidbody.pose.orientation.z;
-                    t.transform.rotation.w = rigidbody.pose.orientation.w;
-
-                    // Broadcast the transform.
-                    tf_broadcaster_->sendTransform(t);
-                }
-            };
-
-            // Subscribe to the rigid_bodies topic. 
+            // Subscribe to the rigid_bodies topic.
             subscription_ = this->create_subscription<mocap4r2_msgs::msg::RigidBodies>(
                 "rigid_bodies", 10,
                 handle_rigid_bodies);
+
+            still_processing = false;
         }
 
 private:
     rclcpp::Subscription<mocap4r2_msgs::msg::RigidBodies>::SharedPtr subscription_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    bool still_processing;
+
+    void handle_rigid_bodies(const std::shared_ptr<mocap4r2_msgs::msg::RigidBodies> msg){
+        if(!still_processing){
+            still_processing = true;
+            for(const auto& rigidbody : msg->rigidbodies) {
+
+                geometry_msgs::msg::TransformStamped t;
+                // Populate the header of the TransformStamped message.
+                t.header.stamp = this->get_clock()->now();
+                t.header.frame_id = "world";
+                t.child_frame_id = rigidbody.rigid_body_name;
+                // Populate the transform data.
+                t.transform.translation.x = rigidbody.pose.position.x;
+                t.transform.translation.y = rigidbody.pose.position.y;
+                t.transform.translation.z = rigidbody.pose.position.z;
+                t.transform.rotation.x = rigidbody.pose.orientation.x;
+                t.transform.rotation.y = rigidbody.pose.orientation.y;
+                t.transform.rotation.z = rigidbody.pose.orientation.z;
+                t.transform.rotation.w = rigidbody.pose.orientation.w;
+
+                // Broadcast the transform.
+                tf_broadcaster_->sendTransform(t);
+            }
+            still_processing = false;
+        }
+    }
 };
 
 int main(int argc, char * argv[])
