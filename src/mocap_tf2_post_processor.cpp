fa4ad0e973e4d7bf504abe6fa49b31ddaf977796
Shockwave PC <iosm.robotics@sa.catapult.org.uk>
2025-12-04 13:21:56 +0000

    rigid bodies callback now calculates relative transform

--- src/mocap_tf2_post_processor.cpp
+++ src/mocap_tf2_post_processor.cpp
@@ -20,6 +20,10 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/pose.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+// Import message types.
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "mocap4r2_msgs/msg/rigid_bodies.hpp"
+#include "mocap4r2_msgs/msg/rigid_body.hpp"
 
 // Define constants.
 const int GRASP_STREAM_ID = 1; // Streaming ID for GRASP
@@ -46,25 +50,30 @@ public:
         relative_twist_publisher_ =
         this->create_publisher<geometry_msgs::msg::TwistStamped>("relative_twist", 10);
 
-        while(!initialised_tf){
-            try{
-                prev_relative_transform = tf_buffer_->lookupTransform(std::to_string(RAFTI_STREAM_ID), std::to_string(GRASP_STREAM_ID), tf2::TimePointZero);
-                // Convert to tf::Transform
-                tf2::fromMsg(prev_relative_transform.transform, prev_relative_tf);
-                prev_relative_transform.header.stamp = this->get_clock()->now();
-                initialised_tf = true;
-            }
-            catch (tf2::TransformException &ex)
-            {
-                RCLCPP_WARN(this->get_logger(), "Initial transform error: %s", ex.what());
-            }
-        }
+        // while(!initialised_tf){
+        //     try{
+        //         prev_relative_transform = tf_buffer_->lookupTransform(std::to_string(RAFTI_STREAM_ID), std::to_string(GRASP_STREAM_ID), tf2::TimePointZero);
+        //         // Convert to tf::Transform
+        //         tf2::fromMsg(prev_relative_transform.transform, prev_relative_tf);
+        //         prev_relative_transform.header.stamp = this->get_clock()->now();
+        //         initialised_tf = true;
+        //     }
+        //     catch (tf2::TransformException &ex)
+        //     {
+        //         RCLCPP_WARN(this->get_logger(), "Initial transform error: %s", ex.what());
+        //     }
+        // }
+
+        prev_relative_transform.header.stamp = this->get_clock()->now();
+
+        subscription_ = this->create_subscription<mocap4r2_msgs::msg::RigidBodies>(
+                "rigid_bodies", 10,
+                std::bind(&MocapTF2Broadcaster::cb_rigid_bodies, this, _1));
 
         // Create timer for periodic processing.
         timer_ = this->create_wall_timer(
             std::chrono::duration<float>(1.0 / PROCESSING_FREQUENCY),
             std::bind(&MocapTF2PostProcessor::process_transforms, this));
-
     }
 
 private:
@@ -76,15 +85,68 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr relative_pose_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr relative_twist_publisher_;
 
+    // Subscribers.
+    rclcpp::Subscription<mocap4r2_msgs::msg::RigidBodies>::SharedPtr rigid_bodies_subscription_;
+
     // Timer.
     rclcpp::TimerBase::SharedPtr timer_;
 
     // store for last transforms
     tf2::Transform prev_relative_tf;
     geometry_msgs::msg::TransformStamped prev_relative_transform;
+    std::shared_ptr<geometry_msgs::msg::TransformStamped> current_subscribed_relative{nullptr};
 
     // bool value to initialise last transform value
     bool initialised_tf{false};
+
+    void cb_rigid_bodies(const std::shared_ptr<mocap4r2_msgs::msg::RigidBodies> msg){
+        geometry_msgs::msg::TransformStamped rafti;
+        geometry_msgs::msg::TransformStamped grasp;
+        for(const auto& rigidbody : last_rigid_bodies->rigidbodies) {
+            geometry_msgs::msg::TransformStamped t;
+            // Populate the header of the TransformStamped message.
+            t.header.stamp = this->get_clock()->now();
+            t.header.frame_id = "world";
+            t.child_frame_id = rigidbody.rigid_body_name;
+            // Populate the transform data.
+            t.transform.translation.x = rigidbody.pose.position.x;
+            t.transform.translation.y = rigidbody.pose.position.y;
+            t.transform.translation.z = rigidbody.pose.position.z;
+            t.transform.rotation.x = rigidbody.pose.orientation.x;
+            t.transform.rotation.y = rigidbody.pose.orientation.y;
+            t.transform.rotation.z = rigidbody.pose.orientation.z;
+            t.transform.rotation.w = rigidbody.pose.orientation.w;
+
+            if(rigidbody.rigid_body_name == std::to_string(RAFTI_STREAM_ID)){
+                rafti = t;
+            } else if(rigidbody.rigid_body_name == std::to_string(GRASP_STREAM_ID)){
+                grasp = t;
+            }
+        }
+
+        tf2::Transform tfA, tfB;
+
+        tf2::fromMsg(rafti.transform, tfA);
+        tf2::fromMsg(grasp.transform, tfB);
+
+        tf2::Transform tfRel = tfA.inverse() * tfB;
+
+        geometry_msgs::msg::TransformStamped rel;
+
+        current_subscribed_relative->transform = tf2::toMsg(rel);
+        current_subscribed_relative->header.stamp = this->get_clock()->now();
+
+        if(!initialised_tf){
+            prev_relative_transform = *current_subscribed_relative;
+            initialised_tf = true;
+        }
+
+        // relative_transform = tf_buffer_->lookupTransform(rafti, grasp, tf2::TimePointZero);
+        // relative_transform.header.stamp = this->get_clock()->now();
+        // // Convert to tf::Transform
+        // tf2::fromMsg(relative_transform.transform, relative_tf);
+
+    }
 
     // Process transforms.
     void process_transforms()
@@ -93,18 +155,26 @@ private:
         geometry_msgs::msg::TransformStamped relative_transform;
         tf2::Transform relative_tf;
 
-        try
-        {
-            relative_transform = tf_buffer_->lookupTransform(std::to_string(RAFTI_STREAM_ID), std::to_string(GRASP_STREAM_ID), tf2::TimePointZero);
-            relative_transform.header.stamp = this->get_clock()->now();
-            // Convert to tf::Transform
-            tf2::fromMsg(relative_transform.transform, relative_tf);
-        }
-        catch (tf2::TransformException &ex)
-        {
-            RCLCPP_WARN(this->get_logger(), "Transform error: %s", ex.what());
+        if(!current_subscribed_relative){
+            RCLCPP_WARN(this->get_logger(), "No subscribed difference yet");
             return;
         }
+
+        // try
+        // {
+        //     relative_transform = tf_buffer_->lookupTransform(std::to_string(RAFTI_STREAM_ID), std::to_string(GRASP_STREAM_ID), tf2::TimePointZero);
+        //     relative_transform.header.stamp = this->get_clock()->now();
+        //     // Convert to tf::Transform
+        //     tf2::fromMsg(relative_transform.transform, relative_tf);
+        // }
+        // catch (tf2::TransformException &ex)
+        // {
+        //     RCLCPP_WARN(this->get_logger(), "Transform error: %s", ex.what());
+        //     return;
+        // }
+
+        relative_transform = *current_subscribed_relative;
+
         // Find latest transform time and retrieve previous transform for twist calculation.
         rclcpp::Time latest_time = relative_transform.header.stamp;
         rclcpp::Time previous_time_target = latest_time - rclcpp::Duration(TWIST_COMPUTE_INTERVAL);
@@ -132,7 +202,6 @@ private:
         // Check for zero or near-zero dt to avoid division by zero or large velocity spikes
         if (dt < MIN_DT) {
             RCLCPP_WARN(this->get_logger(), "Insufficiently large time difference between transforms for twist computation (dt = %g s). Skipping velocity calculation.", dt);
-
             return;
         }
 
